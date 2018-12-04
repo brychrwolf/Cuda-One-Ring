31f47d118ff2d2ba06303637c4eb2069dc44364a
Bryan Wolfford <bryan@wolfford.com>
2018-12-05 00:41:44 +0100

    synthetic_hexagon_DDR SOLVED!

--- example_meshes/synthetic_hexagon_Dirac_delta_function.cpp
+++ example_meshes/synthetic_hexagon_Dirac_delta_function.cpp
@@ -6,6 +6,7 @@
 #include <array>
 #include <vector>
 
+const bool DEBUG = false;//true;
 const double ZERO_TOL = 1e-8;
 const double PI = 4*std::atan(1);
 int globalVertex = 1;
@@ -58,7 +59,6 @@ int main(int ac, char** av){
 	int cornerDist = 1;
 	double centerDist = std::sqrt(3) * cornerDist;
 
-	int numHexes = calculateNumHexes(numRings);
 	int numVertices = calculateNumVertices(numRings);
 	int numFaces = calculateNumFaces(numRings);
 
@@ -68,8 +68,7 @@ int main(int ac, char** av){
 	double cursor[2];
 	double center[2];
 	double corner[2];
-	
-	int hexIdx = 0;
+
 	int vertexIdx = 0;
 	std::vector<int> centers;
 
@@ -94,9 +93,10 @@ int main(int ac, char** av){
 	centers.push_back(vertexIdx++);
 	ply_outfile << "0 0 0 1" << std::endl;
 
-	//Write corners of first hexagon
-	for(int i = 0; i < 6; i++){
-		getPoint(cornerAngles[i], center, cornerDist, corner);
+	//Write corners of first hexagon (start with last two angles)
+	int offset = 2;
+	for(int i = 0+offset; i < 6+offset; i++){
+		getPoint(cornerAngles[i%6], center, cornerDist, corner);
 		writePoint(ply_outfile, corner);
 		vertexIdx++; //Hex corners are not centers
 	}
@@ -122,67 +122,105 @@ int main(int ac, char** av){
 			}
 		}
 	}
+	
+	if(DEBUG) for(size_t i = 0; i < centers.size(); i++)
+		std::cerr << "centers[" << i << "] " << centers[i] << std::endl;
 
 	//Triangles
 	int numHexesOnRing;
+	int globalHexIndex = 0;
+	int n2Index = 0;
 	//Write triangles of middle hex
 	int prev_maxVertex = 0;
-	int prev_maxCenter = 0;
-	int prev_minCenter = 0;
+	//int prev_maxCenter = 0;
+	//int prev_minCenter = 0;
 	int maxVertex = 6;
-	int maxCenter = 0;
-	int minCenter = 0;
+	//int maxCenter = 0;
+	//int minCenter = 0;
 
-	bool prev_isOnCorner = false;
 	bool isOnCorner = false;
-	int prev_hexCenter = 0;
+	//int prev_hexCenter = 0;
 	int hexCenter = 0;
 
-	int n1, n2, n3, n4;
+	int prev_n2 = 0;
+	int n2 = 0; //int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
 	int p1 = 1, p2 = 2, p3 = 3, p4 = 4, p5 = 5, p6 = 6;
 	writeTriangles(ply_outfile, hexCenter, p1, p2, p3, p4, p5, p6);
+	globalHexIndex++;
 
 	//Write triangles for each hex in each side of each ring
 	for(int ring = 1; ring <= numRings; ring++){
 		numHexesOnRing = 6*ring;
 		prev_maxVertex = maxVertex;
-		prev_maxCenter = maxCenter;
-		prev_minCenter = minCenter;
+		//prev_maxCenter = maxCenter;
+		//prev_minCenter = minCenter;
 		maxVertex = calculateNumVertices(ring)-1;
-		maxCenter = maxVertex-3;
-		minCenter = prev_maxVertex+1;
-		for(int hexIdx = 0; hexIdx < numHexesOnRing; hexIdx++){
-			prev_isOnCorner = isOnCorner;
-			isOnCorner = ((ring == 1) || ((hexIdx+1) % ring) == 0);
-			prev_hexCenter = hexCenter;
-			if(prev_isOnCorner){
-				hexCenter = ((hexIdx == 0) ? (prev_maxVertex + 1) : (prev_hexCenter + 4));
-				n1 = 0;
-				n2 = 0;
-				n3 = 0;
-				n4 = 0;
-				p1 = hexCenter+1;
-				p2 = hexCenter+2;
-				p3 = hexCenter+3;
-				p4 = 4;
-				p5 = 5;
-				p6 = 6;
-			}else{
-				hexCenter = ((hexIdx == 0) ? (prev_maxVertex + 1) : (prev_hexCenter + 3));
-				n1 = 0;
-				n2 = 0;
-				n3 = 0;
-				n4 = 0;
-				p1 = hexCenter+1;
-				p2 = hexCenter+2;
-				p3 = 3;
-				p4 = 4;
-				p5 = 5;
-				p6 = 6;		
-			}
-			std::cerr << "ring " << ring << " hexIdx " << hexIdx << " isOnCorner " << isOnCorner << " hexCenter " << hexCenter << std::endl;
+		//maxCenter = maxVertex-3;
+		//minCenter = prev_maxVertex+1;
+		if(DEBUG){
+			std::cerr << "ring " << ring << " numHexesOnRing " << numHexesOnRing << " maxVertex " << maxVertex << std::endl;
+			std::cerr << "prev_maxVertex " << prev_maxVertex << std::endl << std::endl;
 		}
-		std::cerr << std::endl;
+		for(int hexIdx = 0; hexIdx < numHexesOnRing; hexIdx++){
+			isOnCorner = ((ring == 1) || ((hexIdx+1) % ring) == 0);
+
+			prev_n2 = n2;
+			//prev_hexCenter = hexCenter;
+			hexCenter = centers[globalHexIndex];
+
+			//		n1 = ((hexIdx == numHexesOnRing - 1) ? (minCenter) : (centers[globalHexIndex + 1]));
+			if(ring == 1){
+					n2 = 0;
+			//		n3 = prev_hexCenter;
+			//		n4 = maxCenter; //doesn't exist		
+					p1 = hexCenter + 1;
+					p2 = hexCenter + 2;
+					p3 = hexCenter + 3;
+					p4 = hexIdx == 0 ? 6 : hexIdx;
+					p5 = hexIdx == 1 ? 6 : p4 - 1;
+					p6 = hexIdx == 0 ? maxVertex : hexCenter - 1;
+			}else if(hexIdx == 0){
+					n2 = (ring > 1 ? centers[++n2Index] : 0);
+			//		n3 = prev_hexCenter;
+			//		n4 = maxCenter; //doesn't exist		
+					p1 = hexCenter + 1;
+					p2 = hexCenter + 2;
+					p3 = (ring > 1 ? n2 + 1 : hexCenter + 3);
+					p4 = prev_maxVertex;
+					p5 = p4 - 1;
+					p6 = maxVertex;
+			}else{
+				if(isOnCorner){
+					n2 = prev_n2;
+			//		n3 = prev_hexCenter;
+			//		n4 = -1; //doesn't exist					
+					p1 = hexCenter + 1;
+					p2 = hexCenter + 2;
+					p3 = hexCenter + 3;
+					p4 = n2 + 2;
+					p5 = p4 - 1;
+					p6 = hexCenter - 1;
+				}else{
+					n2 = centers[++n2Index];
+			//		n3 = prev_n2;
+			//		n4 = prev_hexCenter;
+					p1 = hexCenter+1;
+					p2 = hexCenter+2;
+					p3 = centers[n2Index] + 1;
+					p4 = n2 - 1;
+					p5 = p4 - 1;
+					p6 = hexCenter - 1;		
+				}
+			}
+			if(DEBUG){
+				std::cerr << "globalHexIndex " << globalHexIndex << " hexIdx " << hexIdx << " isOnCorner " << isOnCorner << " hexCenter " << hexCenter << std::endl;
+				//std::cerr << "   n1 " << n1 << " n2 " << n2 << " n3 " << n3 << " n4 " << n4 << std::endl;
+				std::cerr << "   p1 " << p1 << " p2 " << p2 << " p3 " << p3 << " p4 " << p4 << " p5 " << p5 << " p6 " << p6 << std::endl;
+			}
+			writeTriangles(ply_outfile, hexCenter, p1, p2, p3, p4, p5, p6);
+			globalHexIndex++;
+		}
+		//std::cerr << std::endl;
 	}
 	ply_outfile.close();
 }
