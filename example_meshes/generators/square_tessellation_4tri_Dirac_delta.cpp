324ff19d5439c704fb3d7731215d45868ae11708
Bryan Wolfford <bryan@wolfford.com>
2018-12-08 00:02:36 +0100

    crated generators dir, renamed synthetic with tesselation, added square 4triangle

---
+++ example_meshes/generators/square_tessellation_4tri_Dirac_delta.cpp
@@ -0,0 +1,50 @@
+#include <fstream>
+#include <string>
+
+int main(int ac, char** av){
+	int radius = 4;//;1e2;//1e5;
+	if(ac > 1) radius = std::stoi(av[1]);
+	int diameter = 2*radius + 1;
+	int numVertices_frame = 4*radius*radius + 4*radius + 1;
+	int numVertices_centers = 4*radius*radius;
+	int numVertices = numVertices_frame + numVertices_centers;
+	int numFaces = 4*(diameter-1)*(diameter-1);
+
+	std::string ply_fileName = "../square_tesselation_4tri_Dirac_delta_"+std::to_string(radius)+".ply";
+	std::ofstream ply_outfile(ply_fileName);
+	
+	ply_outfile << "ply" << std::endl
+			<< "format ascii 1.0" << std::endl
+			<< "comment A synthetic square, subdivided by triangles, with a Dirac delta function applied." << std::endl
+			<< "comment made by Bryan Wolfford" << std::endl
+			<< "element vertex " << numVertices << std::endl
+			<< "property int x" << std::endl
+			<< "property int y" << std::endl
+			<< "property int z" << std::endl
+			<< "property float quality" << std::endl
+			<< "element face " << numFaces << std::endl
+			<< "property list uchar int32 vertex_indices" << std::endl
+			<< "end_header" << std::endl;
+
+	//write frame vertices (each square is 2x2)
+	for(long i = -2*radius; i < 2*radius+1; i+=2)
+		for(long j = -2*radius; j < 2*radius+1; j+=2)
+			ply_outfile << i << " " << j << " 0 " << (i==0&&j==0?1:0) << std::endl;
+	//write middle vertices
+	for(long i = -2*radius+1; i < 2*radius; i+=2)
+		for(long j = -2*radius+1; j < 2*radius+1; j+=2)
+			ply_outfile << i << " " << j << " 0 " << (i==0&&j==0?1:0) << std::endl;
+	//write faces
+	int row = 0;
+	for(long i = 0; i < numVertices_centers; i++){
+		if(i > 0 && i % (2*radius) == 0) row++;
+		int center = i+numVertices_frame;
+		int a = i+row;
+		int b = a+1, c=a+diameter, d=c+1;
+		ply_outfile << "3 " << center << " " << a << " " << c << std::endl;
+		ply_outfile << "3 " << center << " " << c << " " << d << std::endl;
+		ply_outfile << "3 " << center << " " << d << " " << b << std::endl;
+		ply_outfile << "3 " << center << " " << b << " " << a << std::endl;
+	}
+	ply_outfile.close();
+}
